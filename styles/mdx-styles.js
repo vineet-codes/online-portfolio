4f6dd7aa0a2c6e68f66de99bbf0de19f3565255f
vineet-codes <vineetwrites@gmail.com>
2022-04-27 10:50:37 +0100

    add stablecoin draft

--- styles/mdx-styles.js
+++ styles/mdx-styles.js
@@ -4,18 +4,26 @@ import styled from "styled-components"
 
 const BlockQuote = styled.blockquote`
   margin:0;
-  padding-top: 0.1em;
-  padding-bottom: 0.1em;
-  padding-left: 1em;
+  padding-top: 0.01em;
+  padding-bottom: 0.01em;
+  padding-left: 0.8em;
 
-  color: #999;
+  color: ${(props) => props.theme.brand.c4};
   font-style: italic;
-  border-left: 0.4em solid ${(props) => props.theme.brand.c1};
+  border-left: 0.2em solid ${(props) => props.theme.brand.c1};
 
+`;
+
+const H2 = styled.h2`  
+  background: ${(props) => props.theme.brand.c1};
+  color: black;
 `;
 
 export const components = {
   blockquote: (props) => {
     return <BlockQuote {...props} />
+  },
+  h2: (props) => {
+    return <H2 {...props} />
   }
 }
